8cc4652a91be4ff6333a1822f1c46b2b8c2f0371
vanbossm <maxime.vanbossel@iut-valence.fr>
2015-10-16 16:30:38 +0200

    Modification of the Javadoc for the class Player

--- iutvalence-java-projet-2015-groupe11-stealthgameproject-master/src/fr/iutvalence/ardechois/stealthgameproject/model/Player.java
+++ iutvalence-java-projet-2015-groupe11-stealthgameproject-master/src/fr/iutvalence/ardechois/stealthgameproject/model/Player.java
@@ -4,49 +4,23 @@ import fr.iutvalence.ardechois.stealthgameproject.exceptions.InvalidPositionExce
 import fr.iutvalence.ardechois.stealthgameproject.view.PlayerGetter;
 
 /**
- * Player that is playing the game.
- * 
  * @author kelemenn
  *
  */
 public class Player implements PlayerGetter
 {
-	/**
-	 * The character position
-	 * 
-	 * @see Position
-	 */
 	private Position position;
 
-	/**
-	 * Constructor whit a position parameter.
-	 * 
-	 * @param position
-	 *            :
-	 * 
-	 */
 	public Player(Position position)
 	{
 		this.position = new Position(position.getX(), position.getY());
 	}
 
-	/**
-	 * Getter for the attribute position.
-	 * 
-	 * @return position : The current character position.
-	 */
 	public Position getPosition()
 	{
 		return this.position;
 	}
 
-	/**
-	 * Move the character to the neighbor square.
-	 * 
-	 * @param direction
-	 * @param map
-	 * @throws InvalidPositionException
-	 */
 	public void move(Direction direction, Map map) throws InvalidPositionException
 	{
 		switch (direction)
