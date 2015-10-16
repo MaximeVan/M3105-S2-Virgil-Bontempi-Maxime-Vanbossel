b61aaaa35a17eac9127ebe52d50f41a4e1823210
vanbossm <maxime.vanbossel@iut-valence.fr>
2015-10-16 16:08:12 +0200

    Modification of the Javadoc for the class Direction

--- iutvalence-java-projet-2015-groupe11-stealthgameproject-master/src/fr/iutvalence/ardechois/stealthgameproject/model/Direction.java
+++ iutvalence-java-projet-2015-groupe11-stealthgameproject-master/src/fr/iutvalence/ardechois/stealthgameproject/model/Direction.java
@@ -1,8 +1,6 @@
 package fr.iutvalence.ardechois.stealthgameproject.model;
 
 /**
- * Choice of direction.
- * 
  * @author kelemenn
  * 
  * @version 0.1.0
@@ -10,32 +8,12 @@ package fr.iutvalence.ardechois.stealthgameproject.model;
 public enum Direction
 
 {
-	/**
-	 * Up direction.
-	 * 
-	 * @value "0,-1"
-	 */
 	UP(0, -1),
 
-	/**
-	 * Down direction.
-	 * 
-	 * @value "0,+1"
-	 */
 	DOWN(0, 1),
 
-	/**
-	 * Left direction.
-	 * 
-	 * @value "-1,0"
-	 */
 	LEFT(-1, 0),
 
-	/**
-	 * right direction.
-	 * 
-	 * @value "1,0"
-	 */
 	RIGHT(1, 0);
 
 	/**
@@ -47,30 +25,17 @@ public enum Direction
 	 */
 	private int y;
 
-	/**
-	 * Player direction, only one character.
-	 */
 	private Direction(int x, int y)
 	{
 		this.x = x;
 		this.y = y;
 	}
 
-	/**
-	 * Getter Position x.
-	 * 
-	 * @return positionX
-	 */
 	public int getX()
 	{
 		return x;
 	}
 
-	/**
-	 * Getter Position y.
-	 * 
-	 * @return positionY
-	 */
 	public int getY()
 	{
 		return y;
