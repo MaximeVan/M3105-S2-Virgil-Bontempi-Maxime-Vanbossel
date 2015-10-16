d285ae8f854e14048362ac431ccc9b9c1bd63cf5
vanbossm <maxime.vanbossel@iut-valence.fr>
2015-10-16 16:15:30 +0200

    Modification of the Javadoc for the class Item

--- iutvalence-java-projet-2015-groupe11-stealthgameproject-master/src/fr/iutvalence/ardechois/stealthgameproject/model/Item.java
+++ iutvalence-java-projet-2015-groupe11-stealthgameproject-master/src/fr/iutvalence/ardechois/stealthgameproject/model/Item.java
@@ -1,89 +1,44 @@
 package fr.iutvalence.ardechois.stealthgameproject.model;
 
 /**
- * Represent an item.
- * 
  * @author vanbossm
  * 
  * */
 public class Item
 {
-	/**
-	 * The Item Position.
-	 * 
-	 * @see Position
-	 * 
-	 * */
 	private Position position;
 
-	/**
-	 * True if the Item is already taken by the Character.
-	 * 
-	 * @see Character
-	 * 
-	 */
-	private boolean taken;
+	private boolean isTaken;
 
-	/**
-	 * Constructor of Item Position
-	 * 
-	 * @param position
-	 * 
-	 */
 	public Item(Position position)
 	{
 		this.position = position;
 	}
 
-	/**
-	 * Getter for the Item Position.
-	 * 
-	 * @return position
-	 * 
-	 */
 	public Position getPosition()
 	{
 		return position;
 	}
 	
-	/**
-	 * Set the item position with the given position.
-	 * @param position
-	 */
 	public void setPosition(Position position)
 	{
 		this.position = new Position(position.getX(), position.getY());
 	}
 
-	/**
-	 * Set the item position with the given x and y coordinates.
-	 * @param x
-	 * @param y
-	 */
 	public void setPosition(int x, int y)
 	{
 		this.position = new Position(x, y);
 		
 	}
 
-	/**
-	 * Getter for the taken boolean.
-	 * 
-	 * @return taken
-	 * 
-	 */
 	public boolean isTaken()
 	{
-		return taken;
+		return isTaken;
 	}
 	
-	/**
-	 * Set the item "state" to taken.
-	 * @param playerPosition
-	 */
 	public void setTaken(Position playerPosition)
 	{
-		this.taken = true;
+		this.isTaken = true;
 		this.position = playerPosition;
 	}
 
